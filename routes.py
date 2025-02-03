3592fffad50cc094f6e1c62d69be9357c035a571
Daniel <vasconcelos.barbosa@academico.ifpb.edu.br>
2025-02-03 05:13:58 -0300

    adicionando import importante

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required
 from models import Evento
 from database import db
